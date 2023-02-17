a01ca3ac2b9eede22aba4d0149852c145a0c5dc4
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-02-17 13:56:43 +0900

    Java Serialization with DIST_SYNC

--- infinispan-embedded/cache-distributed/src/main/java/org/infinispan/tutorial/simple/distributed/InfinispanDistributed.java
+++ infinispan-embedded/cache-distributed/src/main/java/org/infinispan/tutorial/simple/distributed/InfinispanDistributed.java
@@ -1,9 +1,8 @@
 package org.infinispan.tutorial.simple.distributed;
 
-import java.util.UUID;
-
 import org.infinispan.Cache;
 import org.infinispan.commons.api.CacheContainerAdmin;
+import org.infinispan.commons.marshall.JavaSerializationMarshaller;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
@@ -14,25 +13,37 @@ public class InfinispanDistributed {
 
    public static void main(String[] args) throws Exception {
       // Setup up a clustered cache manager
-      GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
-      // Initialize the cache manager
+       GlobalConfigurationBuilder global = new GlobalConfigurationBuilder();
+       global.serialization()
+               .marshaller(new JavaSerializationMarshaller())
+               .allowList()
+               .addClasses(Person.class);
+       global.transport().defaultTransport();
+
+       // Initialize the cache manager
       DefaultCacheManager cacheManager = new DefaultCacheManager(global.build());
       //Create cache configuration
       ConfigurationBuilder builder = new ConfigurationBuilder();
       builder.clustering().cacheMode(CacheMode.DIST_SYNC);
       // Obtain a cache
-      Cache<String, String> cache = cacheManager.administration().withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
+      Cache<Person, String> cache = cacheManager.administration().withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
             .getOrCreateCache("cache", builder.build());
 
       // Store the current node address in some random keys
-      for (int i = 0; i < 10; i++) {
-         cache.put(UUID.randomUUID().toString(), cacheManager.getNodeAddress());
+      for (int i = 0; i < 1; i++) {
+         cache.put(new Person("John" + i), cacheManager.getNodeAddress());
       }
       // Display the current cache contents for the whole cluster
+      System.out.println("------ Display the current cache contents for the whole cluster");
       cache.entrySet().forEach(entry -> System.out.printf("%s = %s\n", entry.getKey(), entry.getValue()));
+
+      Thread.sleep(10000l);
+      
+      
       // Display the current cache contents for this node
       // Note: By default numOwners=2, so in a cluster with 2 nodes, each node owns all the keys:
       // some of the keys as "primary owner" and some keys as "backup owner"
+      System.out.println("------ Display the current cache contents for this node");
       cache.getAdvancedCache().withFlags(Flag.CACHE_MODE_LOCAL).entrySet()
             .forEach(entry -> System.out.printf("%s = %s\n", entry.getKey(), entry.getValue()));
       // Stop the cache manager and release all resources
